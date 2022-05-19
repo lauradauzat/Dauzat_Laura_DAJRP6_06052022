5c689566fbcccf4acba149c9a61186a9bd463cb4
Laura Dauzat <63320636+lauradauzat@users.noreply.github.com>
2022-05-19 17:28:47 +0200

    fetch data accueil  and print data acceuil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,23 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.innerHTML = `<img src="${picture}" alt="${name}">
+        <h2>${name}</h2>
+        <h3>${city}, ${country}</h3>
+        <p>${tagline}</p>
+        <span>${price}€/jour</span>
+        `; 
+
+        // const img = document.createElement( 'img' );
+        // img.setAttribute("src", picture)
+        // const h2 = document.createElement( 'h2' );
+        // h2.textContent = name;
+        // article.appendChild(img);
+        // article.appendChild(h2);
         return (article);
     }
     return { name, picture, getUserCardDOM }
